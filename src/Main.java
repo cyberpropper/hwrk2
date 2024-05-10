4eb76f344a92b4bf3c1cc17a0e14eeb32ca4e9ed
cyberpropper <osankin2002@mail.ru>
2024-05-10 22:58:35 +0700

    Я исправил, но комментарии не нашел

--- src/Main.java
+++ src/Main.java
@@ -9,34 +9,34 @@ public class Main {
 
         var friend = 19;
         System.out.println(friend);
-        var friend2 = friend + 2;
-        System.out.println(friend2);
-        var friend3 = friend2 / 7;
-        System.out.println(friend3);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
 
         var frog = 3.5;
         System.out.println("жаба1 " + frog);
-        var frog2 = frog * 10;
-        System.out.println("жаба2 " + frog2);
-        var frog3 = frog2 / 3.5;
-        System.out.println("жаба3 " + frog3);
-        var frog4 = frog3 + 4;
-        System.out.println("жаба4 " + frog4);
+        frog = frog * 10;
+        System.out.println("жаба2 " + frog);
+        frog = frog / 3.5;
+        System.out.println("жаба3 " + frog);
+        frog = frog + 4;
+        System.out.println("жаба4 " + frog);
 
         var boxer1 = 78.2;
         var boxer2 = 82.7;
         var allBoxer = boxer1 + boxer2;
         System.out.println("общий вес " + allBoxer );
-        var diff = boxer2 - boxer1;
-        System.out.println("разница " + diff);
-        var rem = boxer2 % boxer1;
-        System.out.println("остаток от деления " + rem );
+        allBoxer = boxer2 - boxer1;
+        System.out.println("разница " + allBoxer);
+        allBoxer = boxer2 % boxer1;
+        System.out.println("остаток от деления " + allBoxer);
 
         var wTime = 640;
         var workers = wTime / 8;
         System.out.println("Всего работников в компании - " + workers);
-        var workersPlus = workers + 94;
-        var wTimeAll = 640 / workersPlus;
-        System.out.println("Если в компании работает " + workersPlus + " человек, то всего " + wTimeAll + " часов работы может быть поделено между сотрудниками");
+        workers = workers + 94;
+        wTime = wTime / workers;
+        System.out.println("Если в компании работает " + workers + " человек, то всего " + wTime + " часов работы может быть поделено между сотрудниками");
     }
 }
